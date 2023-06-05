3701f440157cca615b15a7642bbfee1ee4f73e03
cod1ng-now <muhammad.axmedov.2000@bk.ru>
2023-06-05 15:03:02 +0500

    cod1ng-now

--- projectapi/settings.py
+++ projectapi/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'projectapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'baza.sqlite3',
     }
 }
 
